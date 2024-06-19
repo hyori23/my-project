474b49b04dedb695d1b84fd3c03395fb26193c0e
hyori23 <162565671+hyori23@users.noreply.github.com>
2024-06-19 15:05:32 +0900

    두더지자바연결

--- script/script.js
+++ script/script.js
@@ -24,3 +24,43 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lottiePlayer = document.getElementById('lottie-1');
+
+  lottiePlayer.addEventListener('click', function () {
+    window.location.href = './grade/1-1/index1-1.html'; // 이동할 페이지의 URL을 넣어주세요.
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lottiePlayer = document.getElementById('lottie-2');
+
+  lottiePlayer.addEventListener('click', function () {
+    window.location.href = './grade/1-2/index1-2.html'; // 이동할 페이지의 URL을 넣어주세요.
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lottiePlayer = document.getElementById('lottie-3');
+
+  lottiePlayer.addEventListener('click', function () {
+    window.location.href = './grade/2-video/indexvideo.html'; // 이동할 페이지의 URL을 넣어주세요.
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lottiePlayer = document.getElementById('lottie-4');
+
+  lottiePlayer.addEventListener('click', function () {
+    window.location.href = './grade/2-Typo/indexTypo.html'; // 이동할 페이지의 URL을 넣어주세요.
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const lottiePlayer = document.getElementById('lottie-5');
+
+  lottiePlayer.addEventListener('click', function () {
+    window.location.href = './grade/2-CD/indexCD.html'; // 이동할 페이지의 URL을 넣어주세요.
+  });
+});
