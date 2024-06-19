37c866e379cf8e8bf5a547265c9ac4fbe3c0447f
hyori23 <162565671+hyori23@users.noreply.github.com>
2024-06-19 15:09:14 +0900

    링크오류

--- script/script.js
+++ script/script.js
@@ -1,13 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   const lotties = document.querySelectorAll('.lottie');
 
-  // 각각의 dotlottie-player에 대해 반복하여 이벤트 리스너 추가
   lotties.forEach(function (lottie) {
     let isReversed = false;
 
-    // 마우스 진입 이벤트 리스너 추가
     lottie.addEventListener('mouseenter', function () {
-      // 역재생 설정
       if (!isReversed) {
         lottie.setDirection(1);
         lottie.play();
@@ -15,9 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     });
 
-    // 마우스 빠져나옴 이벤트 리스너 추가
     lottie.addEventListener('mouseleave', function () {
-      // 원래 상태로 리셋
       lottie.setDirection(-1);
       lottie.stop();
       isReversed = false;
